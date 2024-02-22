e00849bf79f804ed101c9307d0cf79e567b2b6e7
ThomasStenholtLaursen <201905236@post.au.dk>
2024-02-22 18:11:36 +0100

    build configurations + netlify adjustments

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,32 @@
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.includes("react-router-dom") || id.includes("react-router")) {
+            return "@react-router";
+          }
+          if (id.includes("lucide-react") || id.includes("lucide")) {
+            return "@lucide-react";
+          }
+          if (id.includes("@radix-ui")) {
+            return "@radix-ui";
+          }
+          if (id.includes("@tanstack")) {
+            return "@tanstack";
+          }
+          if (id.includes("recharts")) {
+            return "@recharts";
+          }
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
